478cd1a146c7647801be573a0e9ff807d80832f9
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-07 11:54:20 +0300

    Sloved task 3

--- lab.py
+++ lab.py
@@ -1,18 +1,26 @@
 # Task 1 : 
-print("Task 1 :  Display all prime numbers within a range")
-from_range = int(input("From :"))
-to_range = int(input("To :"))
+# print("Task 1 :  Display all prime numbers within a range")
+# from_range = int(input("From :"))
+# to_range = int(input("To :"))
 
-def check_is_prime(x):
-    flag = True
+# def check_is_prime(x):
+#     flag = True
     
-    for i in range(2,x):
-        if x % i == 0:
-            flag = False
+#     for i in range(2,x):
+#         if x % i == 0:
+#             flag = False
             
-    return flag
+#     return flag
 
-for i in range(from_range,to_range+1):
-    if check_is_prime(i):
-        print(i)
+# for i in range(from_range,to_range+1):
+#     if check_is_prime(i):
+#         print(i)
+
+
+# Task 2 :
+
+def show_student(name, gpa=0):
     
+    print (name ," Has " ,gpa  )
+
+show_student("saad",3.25)
