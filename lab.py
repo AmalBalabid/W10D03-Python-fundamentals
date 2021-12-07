b6a5c80023753b3671ea80868c644251a31ab2f2
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-07 11:41:20 +0300

    Solved task1

---
+++ lab.py
@@ -0,0 +1,18 @@
+# Task 1 : 
+print("Task 1 :  Display all prime numbers within a range")
+from_range = int(input("From :"))
+to_range = int(input("To :"))
+
+def check_is_prime(x):
+    flag = True
+    
+    for i in range(2,x):
+        if x % i == 0:
+            flag = False
+            
+    return flag
+
+for i in range(from_range,to_range+1):
+    if check_is_prime(i):
+        print(i)
+    
